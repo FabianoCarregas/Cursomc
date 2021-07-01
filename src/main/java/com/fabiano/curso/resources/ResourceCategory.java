04361e00830b9bc9ce483c7088621d8a19e018ca
FabianoCarregas <fabianofac_@hotmail.com>
2021-07-01 10:56:37 -0300

    Data base H2 automatic data update

--- src/main/java/com/fabiano/curso/resources/ResourceCategory.java
+++ src/main/java/com/fabiano/curso/resources/ResourceCategory.java
@@ -18,7 +18,7 @@ public class ResourceCategory {
 	public List<Category> listar() {
 		
 		Category cat1 = new Category(1, "Informatic");
-		Category cat2= new Category(1, "Office");
+		Category cat2= new Category(2, "Office");
 		
 		List<Category> list= new ArrayList<>();
 		list.add(cat1);
