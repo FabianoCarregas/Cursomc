c5814e81440d9a52d5bad9e92d9243cd9ee396c8
FabianoCarregas <fabianofac_@hotmail.com>
2021-07-01 09:55:28 -0300

    first class testing- category

--- src/main/java/com/fabiano/curso/resources/ResourceCategory.java
+++ src/main/java/com/fabiano/curso/resources/ResourceCategory.java
@@ -1,8 +1,13 @@
 package com.fabiano.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fabiano.curso.domain.Category;
 
 
 @RestController
@@ -10,7 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ResourceCategory {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando!";
+	public List<Category> listar() {
+		
+		Category cat1 = new Category(1, "Informatic");
+		Category cat2= new Category(1, "Office");
+		
+		List<Category> list= new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
 }
